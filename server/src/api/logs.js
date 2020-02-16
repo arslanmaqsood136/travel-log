344461e536b72e01fe5a82243b36826fbb367049
Arslan <arslanmaqsood136@gmail.com>
2020-02-16 22:11:57 +0500

    Basic API setup with Express and Mongoose

---
+++ server/src/api/logs.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+
+const router = Router();
+const LogEntry = require('../models/LogEntry');
+
+router.post('/', async (req, res, next) => {
+  try {
+    const logEntry = new LogEntry(req.body);
+    const createdEntry = await logEntry.save();
+    res.json(createdEntry);
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res.status(422);
+    }
+    next(error);
+  }
+});
+
+router.get('/', async (req, res, next) => {
+  try {
+    const logEntries = await LogEntry.find();
+    res.json(logEntries);
+  } catch (error) {
+    next(error);
+  }
+});
+module.exports = router;
